2b5fd9b6cdc3378b984ba2119cef0b2c700f3ac6
Araujo <cauaraujo@deloitte.com>
2025-08-06 11:59:05 -0300

    start: iniciando projeto

---
+++ src/main/java/com/ia_rag_application/RagApplication.java
@@ -0,0 +1,13 @@
+package com.ia_rag_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ia_rag_application/RagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia_rag_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
